f1a16bd5be73d4decec95b943654b5cf3e616bce
ethen09 <etheny09@gmail.com>
2025-02-06 16:29:09 -0800

    Update urls.py

--- base/urls.py
+++ base/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views, name="home"),
+    path('',views.home, name="home"),
     path('room/', views.room,name ="room"),
 
 ]
